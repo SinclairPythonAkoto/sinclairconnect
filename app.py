1cc42ba80f6fc8f900a61a95c0015746c6c3ad9e
SinclairPythonAkoto <sinclair.python@gmail.com>
2019-08-24 20:43:24 +0100

    file update

--- app.py
+++ app.py
@@ -35,11 +35,12 @@ def textMe():
 		txt = request.form.get('sendText')
 		sender = request.form.get('senderName')
 		from clockwork import clockwork
-		api = clockwork.API(f'1ddc9fd9717efbc300deb3d1753e182eb296d918',from_name='{sender}')
+		api = clockwork.API('1ddc9fd9717efbc300deb3d1753e182eb296d918')
 
 		message = clockwork.SMS(
 		    to = '447481790498',
-		    message = f'{txt}',from_name={sender})
+		    message = f'{txt}',
+		    from_name=f'{sender}')
 
 		response = api.send(message)
 
